79ec11e75d2f62eb695a6b0084318c5317ea4eed
amhb1985 <amirhabibi219@gmail.com>
2023-12-24 10:50:21 +0100

    changing2iosV09

---
+++ mvc-ifyme-cap/mvc-ifyme-cap/Model/AppleProduct.swift
@@ -0,0 +1,8 @@
+//
+//  AppleProduct.swift
+//  mvc-ifyme-cap
+//
+//  Created by Amir Habibi on 24.12.23.
+//
+
+import Foundation
---
+++ mvc-ifyme-cap/mvc-ifyme-cap/View/CustomPrettyView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomPrettyView.swift
+//  mvc-ifyme-cap
+//
+//  Created by Amir Habibi on 24.12.23.
+//
+
+import UIKit
+
+class CustomPrettyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
